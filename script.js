873084e84aef83f2d07bc0a73b2508f3e0d223de
Christy Stany <christystany1@gmail.com>
2026-03-26 11:27:10 +0530

    final fix form

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
-const form = document.getElementById("contactForm");
+console.log("JS LOADED");
 
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
+window.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("contactForm");
 
-  const data = {
-    name: document.getElementById("name").value,
-    email: document.getElementById("email").value,
-    message: document.getElementById("message").value
-  };
+  console.log("Form:", form); // should NOT be null
 
-  try {
-    await fetch("https://portfolio-89dd.onrender.com/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data)
-    });
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    console.log("FORM SUBMITTED");
 
-    alert("Message sent successfully 🚀");
-  } catch (err) {
-    alert("Error sending message ❌");
-  }
+    const data = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value
+    };
+
+    try {
+      await fetch("https://portfolio-89dd.onrender.com/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      });
+
+      alert("Message sent ✅");
+    } catch (err) {
+      console.error(err);
+      alert("Error ❌");
+    }
+  });
 });
